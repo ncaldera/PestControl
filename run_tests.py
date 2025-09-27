26fb07d40e03a326d826f83aa772882d1e18487f
Maya <mnoren@umich.edu>
2025-09-27 19:01:50 -0400

    hi

--- run_tests.py
+++ run_tests.py
@@ -23,50 +23,88 @@ output: in terminal
 run again option? [did it work y/n -> rerun prompt]
 
 '''
-import json, shuntil
+from colorama import Fore, Back, Style, init
+import json, shuntil, subprocess, os
 
-def main()
+def main():
 
-    success = False
+    for i in range(num_loops):
+        #TODO run gemini and get input
+        
+        with open("combined.json") as f:
+            input_data = json.load(f)
 
-    #TODO figure out num loops
 
-    for i in range(num_loops)
-        #TODO run gemini and get input -> json: fixed code, patch, why (string), line nums(int)
-        with open("input.json") as f:
-        input_data = json.load(f)
+        original_code_path = input_data["original_code_path"] #for next input
+        patch = input_data["patch_path"] #why, line nums, num trys, context files
 
-        patch = input_data["patch"]
-        why = input_data["why"]
+        tests = input_data["tests"]
 
-        tests = input_data["tests"]#TODO change? 
-        target_file = input_data["target_file"], num loops(int) #TODO change? 
+        patched_code = input_data["fixed_code_path"] # just fixed code
+        path_to_txt = Path(patched_code)
+        patched_code_py = path_to_txt.with_suffix(".py") #path to executable 
+        
+        # copy text from txt to executabe
+        with open(path_to_txt, "r", encoding="utf-8") as f:
+            code = f.read()
 
-        #TODO make fixed code -> executable
-        #TODO run tests
+        with open(patched_code_py, "w", encoding="utf-8") as f:
+            f.write(code)
 
-        if #TODO test passes
+        # run tests
+        workdir = str(patched_code_py.parent)
+        result = subprocess.run(["pytest"] + tests, cwd = workdir)
+
+        if result.returncode == 0 # all tests passed
             success = True
             break
 
 
-    if success
-        print(Fore.green + "Generated fix successful!")
+
+
+    success = False
+
+    #!FIRST ITERATION
+    with open("combined.json") as f:
+            input_data = json.load(f)
+    
+    original_code_path = input_data["original_code_path"] #for next input
+    context_files = input_data["context_files"]
+
+    
+
+    #!NEXT ITERATIONS (LOOP)
+
+    for i in range(num_loops):
+        #TODO run gemini and get new input
+        
+        with open("combined.json") as f:
+            input_data = json.load(f)
+
+
+
+
+    #!PRINTING
+    if success:
+        print(Fore.GREEN + "Generated fix successful!")
+
         print()
-        print("Suggested patch: ")
+        #TODO parse patch and break up in order to print better
+
+        print(Style.BRIGHT + "Suggested patch: ")
         width = shutil.get_terminal_size().columns
-        print(f"line {start_line}" + "-" * (width - 8))
-        print(patch)
+        print(Style.RESET_ALL + f"line {start_line}" + "-" * (width - 8))
+        print(Fore.CYAN + patch)
         print(f"line {end_line}" + "-" * (width - 8))
+
         print()
-        print("Patch description: ")
-        print(why)
 
+        print(Style.BRIGHT + "Patch description: ")
+        print(Style.RESET_ALL + why)
     else:
-        print(Fore.red + "All generated patches failed :(")
+        print(Fore.RED + "All generated patches failed :(")
 
-    again = input("Run again? [y/n]: ")
-
+    again = input(Back.WHITE + "Run again? [y/n]: ")
     if again.lower().startswith("y"):
         main()
     else:
